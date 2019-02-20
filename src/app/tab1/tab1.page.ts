a23c790574ec98dfe4ffb2ef8d4fba32c1ccc7be
joffrey <joffrey-lhermitte@hotmail.fr>
2019-02-20 16:42:21 +0100

    correction couleur

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,27 +11,8 @@ export class Tab1Page {
 
   currentDate = new Date();
 
-  constructor (public actionSheetController: ActionSheetController, private router: Router) {
+  constructor () {
 
   }
 
-  async action() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Outils',
-      buttons: [{
-        text: 'IMC',
-        icon: 'settings',
-        handler: () => {
-          this.router.navigateByUrl('tabs/imc');
-        }
-      }, {
-        text: 'IMG',
-        icon: 'settings',
-        handler: () => {
-          this.router.navigateByUrl( 'tabs/img');
-        }
-      }]
-    });
-    await actionSheet.present();
-  }
 }
